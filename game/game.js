c734f9b2d496cd46aeac21e83b6219094b99239c
Vytautas Jakutis <jakutis@wix.com>
2014-11-08 14:28:50 +0200

    do not initialize database

--- game/game.js
+++ game/game.js
@@ -102,15 +102,3 @@ if (Meteor.isClient) {
         }
     });
 }
-
-if (Meteor.isServer) {
-    Meteor.startup(function () {
-        if(Stories.find().count() === 0) {
-            Stories.insert({
-                lock: null,
-                sentences: [],
-                words: []
-            })
-        }
-    });
-}
